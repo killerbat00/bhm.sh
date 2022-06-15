0ef98d55713811bf26843341f5cb9ae7b506ac02
brian houston morrow <bhm@brianmorrow.net>
2022-06-14 22:49:56 -0500

    Give debug binaries a different name.

--- build.sh
+++ build.sh
@@ -6,13 +6,13 @@ RLS_OUTPUT_FILE=bin/bhm.sh
 SRC_FILE=main.nim
 
 if [[ "$MODE" == "dev" ]]; then
-	nim c -o:$OUTPUT_FILE $2 $SRC_FILE
+	nim c -o:$DEV_OUTPUT_FILE $2 $SRC_FILE
 elif [[ "$MODE" == "DEV" ]]; then
-	nim c -o:$OUTPUT_FILE $2 $SRC_FILE
+	nim c -o:$DEV_OUTPUT_FILE $2 $SRC_FILE
 elif [[ "$MODE" == "rls" ]]; then
-	nim c -o:$OUTPUT_FILE -d:release $2 $SRC_FILE
+	nim c -o:$RLS_OUTPUT_FILE -d:release $2 $SRC_FILE
 elif [[ "$MODE" == "RLS" ]]; then
-	nim c -o:$OUTPUT_FILE -d:release $2 $SRC_FILE
+	nim c -o:$RLS_OUTPUT_FILE -d:release $2 $SRC_FILE
 else
 	echo "Usage:"
 	echo "  ./build.sh {dev|rls} [-r]"
