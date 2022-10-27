539823ea5b11812f77ca50b651f170dce25b3d78
brian houston morrow <bhm@brianmorrow.net>
2022-10-26 23:33:52 -0500

    Update build script.\nAdd dry run command and speed optimize build.

--- build.sh
+++ build.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
 
-MODE=$1
-DEV_OUTPUT_FILE=bin/bhm.sh-DEV
-RLS_OUTPUT_FILE=bin/bhm.sh
 SRC_FILE=main.nim
+RLS_OUTPUT_FILE=bin/bhm.sh
+DEV_OUTPUT_FILE="${RLS_OUTPUT_FILE}"-DEV
+DRY_RUN=0
+MODE=$1
+RUN=$2
 
-if [[ "$MODE" == "dev" ]]; then
-	nim c -o:$DEV_OUTPUT_FILE $2 $SRC_FILE
-elif [[ "$MODE" == "DEV" ]]; then
-	nim c -o:$DEV_OUTPUT_FILE $2 $SRC_FILE
-elif [[ "$MODE" == "rls" ]]; then
-	nim c -o:$RLS_OUTPUT_FILE -d:release $2 $SRC_FILE
-elif [[ "$MODE" == "RLS" ]]; then
-	nim c -o:$RLS_OUTPUT_FILE -d:release $2 $SRC_FILE
+if [[ "${MODE}" == "-t" ]]; then
+    DRY_RUN=1
+    MODE=$2
+    RUN=$3
+fi
+
+if [[ "${MODE,,}" == "dev" ]]; then
+    if [[ "$DRY_RUN" == "" ]]; then
+	    nim c -o:$DEV_OUTPUT_FILE $RUN $SRC_FILE
+    else
+        echo "nim c -o:${DEV_OUTPUT_FILE} ${RUN} ${SRC_FILE}"
+    fi
+elif [[ "${MODE,,}" == "rls" ]]; then
+    if [[ "$DRY_RUN" == "" ]]; then
+	    nim c -o:$RLS_OUTPUT_FILE -d:release --opt:speed $RUN $SRC_FILE
+    else
+        echo "nim c -o:${RLS_OUTPUT_FILE} -d:release --opt:speed ${RUN} ${SRC_FILE}"
+    fi
 else
 	echo "Usage:"
-	echo "  ./build.sh {dev|rls} [-r]"
+	echo "  ./build.sh [-t] {dev|rls} [-r]"
 	echo ""
 	echo "  Options:"
-	echo "    dev - build development version"
-	echo "    rls - build release version"
-	echo "    -r  - run after building"
+    echo "    -t    - dry run, echos commands"
+    echo "    mode:"
+	echo "      dev - build development version"
+	echo "      rls - build release version"
+	echo "    -r    - run after building"
 fi
 
