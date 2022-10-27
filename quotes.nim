a474d7d8294ba09c2a0e81cee91f93f8a09a3ce8
brian houston morrow <bhm@brianmorrow.net>
2022-10-26 20:44:19 -0500

    Update file embedding behavior. Extract quotes and titles to their own files to clean up main.

---
+++ quotes.nim
@@ -0,0 +1,81 @@
+const quotes* = [
+    ("Every era puts invisible shackles on those who have lived through it, and I can only dance in my chains.",
+        "Liu Cixin",
+        "<i>The Three-Body Problem</i>"),
+    ("""I must not fear.
+<br>Fear is the mind-killer.
+<br>Fear is the little-death that brings total obliteration.
+<br>I will face my fear.
+<br>I will permit it to pass over me and through me.
+<br>And when it has gone past, I will turn the inner eye to see its path.
+<br>Where the fear has gone there will be nothing. Only I will remain.""",
+        "Frank Herbert",
+        "<i>Dune</i>"),
+    ("The mystery of life isn't a problem to solve, but a reality to experience.",
+        "Frank Herbert",
+        "<i>Dune</i>"),
+    ("Deep in the human unconscious is a pervasive need for a logical universe that makes sense. But the real universe is always one step beyond logic.",
+        "Frank Herbert",
+        "<i>Dune</i>"),
+    ("It is impossible to live in the past, difficult to live in the present and a waste to live in the future.",
+        "Frank Herbert",
+        "<i>Dune</i>"),
+    ("He who controls the spice controls the universe.",
+        "Frank Herbert",
+        "<i>Dune</i>"),
+    ("Sometimes I can hear my bones straining under the weight of all the lives I'm not living.",
+        "Jonathan Safran Foer",
+        "<i>Extremely Loud & Incredibly Close</i>"),
+    ("I regret that it takes a life to learn how to live.",
+        "Jonathan Safran Foer",
+        "<i>Extremely Loud & Incredibly Close</i>"),
+    ("Songs are as sad as the listener.",
+        "Jonathan Safran Foer",
+        "<i>Extremely Loud & Incredibly Close</i>"),
+    ("So many people enter and leave your life! Hundreds of thousands of people! You have to keep the door open so they can come in! But it also means you have to let them go!",
+        "Jonathan Safran Foer",
+        "<i>Extremely Loud & Incredibly Close</i>"),
+    ("My life story is the story of everyone I've ever met.",
+        "Jonathan Safran Foer",
+        "<i>Extremely Loud & Incredibly Close</i>"),
+    ("One morning, over at Elizabeth’s beach house, she asked me if I’d rather go water-skiing or lay out. And I realized that not only did I not want to answer THAT question, but I never wanted to answer another water-sports question, or see any of these people again for the rest of my life.",
+        "Anthony",
+        "<i>Bottle Rocket</i>"),
+    ("I know, honey. Look at the map. We go your way, that’s about four inches. We go my way, it’s an inch and a half. You wanna pay for the extra gas?",
+        "Steve Zissou",
+        "<i>The Life Aquatic with Steve Zissou</i>"),
+    ("Why a fox? Why not a horse, or a beetle, or a bald eagle? I’m saying this more as, like, existentialism, you know? Who am I? And how can a fox ever be happy without, you’ll forgive the expression, a chicken in its teeth?",
+        "Mr. Fox",
+        "<i>Fantastic Mr. Fox</i>"),
+    ("You know I'm not big on apologizing. So I'll just skip it if it's all the same to you. [OK.] Anyway, I'm sorry.",
+        "Steve Zissou",
+        "<i>The Life Aquatic with Steve Zissou</i>"),
+    ("It's a luscious mix of words and tricks that let us bet when you know we should fold.",
+        "The Shins",
+        """"Caring is Creepy""""),
+    ("Faced with the dodo's conundrum I felt like I could just fly, but nothing happened every time I'd try.",
+        "The Shins",
+        """"Australia""""),
+    ("""Turn me back into the pet I was when we met.
+<br>I was happier then with no mind-set.""",
+        "The Shins",
+        """"New Slang""""),
+    ("""Held to the past, too aware of the pending
+<br>Chill as the dawn breaks and finds us up for sale
+<br>Enter the fog another low road descending
+<br>Away from the cold lust, you house and summertime.""",
+        "The Shins",
+        """"The Past and Pending""""),
+    ("How strange it is to be anything at all.",
+        "Neutral Milk Hotel",
+        """"In the Aeroplane Over the Sea""""),
+    ("""Two-headed boy
+<br>She is all you could need
+<br>She will feed you tomatoes and radio wire
+<br>And retire to sheets safe and clean
+<br>But don't hate her when she gets up to leave.""",
+        "Neutral Milk Hotel",
+        """"Two Headed Boy Pt. II""""),
+]
+
+export quotes
---
+++ titles.nim
@@ -0,0 +1,28 @@
+const titles* = [
+    "cabin in the woods",
+    "slice of the internet",
+    "home on the World Wide Web",
+    "bytes",
+    "virtual space",
+    "website",
+    "page",
+    "country corner",
+    "internet of things",
+    "is the place to be",
+    "personal digital destination",
+    "home for wayward dogs",
+    "home of horrors (NOT scary)"
+]
+
+const chyrons* = [
+    "Now in technicolor",
+    "...oh, this is still here?",
+    "fueled by Diet Mtn Dew",
+    "allegedly",
+    "dog hair now included!",
+    "enjoy your stay!",
+    "FREE bits AND bytes!",
+    "wow, what a great audience"
+]
+
+export titles, chyrons
