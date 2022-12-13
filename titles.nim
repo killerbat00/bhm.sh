d4946ceea5120ebfdfaad049bac44901e503d274
brian houston morrow <bhm@brianmorrow.net>
2022-12-12 23:45:14 -0600

    Content: Potential GPT-3 generated titles.

--- titles.nim
+++ titles.nim
@@ -16,6 +16,70 @@ const titles* = [
     "cheeseburger in paradise",
 ]
 
+# candidates from GPT-3
+# "Brian's Tower of Babel"
+# "Brian's Web of Confusion"
+# "Brian's Byte-Sized Bites"
+# "Brian's Matrix of Dreams"
+# "Brian's Kingdom of Coding"
+# "Brian's Virtual Vacation"
+# "Brian's Online Oasis"
+# "Brian's Interwebs of Intrigue"
+# "Brian's Portal to Programming"
+# "Brian's Labyrinth of Links"
+# "Brian's Interweb Wonderworld"
+# "Brian's Digital Palace"
+# "Brian's Home of High-Tech Hijinks"
+# "Brian's Cyber-Candyland"
+# "Brian's Online Universe"
+# "Brian's Starship Surfing"
+# "Brian's Web of Wonders"
+# "Brian's Galactically Good Pages"
+# "Brian's Intergalactic Bytes"
+# "Brian's Virtual Galaxy"
+# "Brian's Extra-Terrestrial Website"
+# "Brian's Interdimensional Page"
+# "Brian's Cosmic Corner"
+# "Brian's Star-Spangled Internet of Things"
+# "Brian's Outer Space is the Place to Be"
+# "Brian's Personal Portal to the Universe"
+# "Brian's Home for Wayward Aliens"
+# "Brian's Home of Happy Aliens"
+# "Brian's Interplanetary Dreamscape"
+# "Brian's Nebular Cheeseburger"
+# "Brian's Babel of Bytes",
+# "Brian's Hitchhiker's Haven on the Web",
+# "Brian's Digital Detour to the Ultimate Answer",
+# "Brian's Home on the Intergalactic Web",
+# "Brian's So Long and Thanks for All the Bytes",
+# "Brian's Personal Portal to the Galaxy",
+# "Brian's Virtual Vogon Village",
+# "Brian's Poetic Pan-Galactic Gargle Blaster",
+# "Brian's Country Corner in Deep Space",
+# "Brian's Interdimensional Dreamscape",
+# "Brian's Home of Horrors (NOT Scary) and Towels",
+# "Brian's Cheeseburger in the Vogon Cafe"
+# "Brian's Galactic Hang-out"
+# "Brian's Warp Zone"
+# "Brian's Infinity and Beyond"
+# "Brian's Inter-galactic Stopover"
+# "Brian's Warp-Speed Webpage"
+# "Brian's Far-Out Page"
+# "Brian's Sci-Fi Haven"
+# "Brian's Out-of-this-World Site"
+# "Brian's Cyber-Universe"
+# "Brian's Hyperdrive Homepage"
+# "Brian's Off-World Colonies"
+# "Brian's Neuromancer's Retreat"
+# "Brian's Snow Crash Oasis"
+# "Brian's Cyberpunk Hideaway"
+# "Brian's Matrix Mania"
+# "Brian's Digital Dystopia"
+# "Brian's Electric Sheep Sanctuary"
+# "Brian's Techno-Wonderland"
+# "Brian's Postmodern Paradise"
+# "Brian's AI Adventureland"
+
 const chyrons* = [
     "Now in technicolor",
     "...oh, this is still here?",
